4f732fde3970a1baab638a3878d79a53ec0eb4b7
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-04 12:25:42 -0300

    build: add sakura.css as a framework

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "../node_modules/sakura.css/css/normalize.css"
+import "../node_modules/sakura.css/css/sakura.css"
 
 Vue.config.productionTip = false
 
