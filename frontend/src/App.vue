e21c3b73e4a060c9518e03a9fcce95396afea7ba
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-04 16:50:37 -0300

    feat: edit and delete tasks

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <Header />
     <form
-      @submit="saveTask">
+      @submit="save">
       <div class="task_name">
         <label 
           for="task_description">Tarefa</label>
@@ -14,12 +14,22 @@
       
       <div class="task_info">
         <div>
-        <label for="date_time">Data e hora</label>
-        <input 
-          id="date_time"
-          type="datetime-local"
-          v-model="taskDateAndTime" />
+          <label for="task_date">Data</label>
+          <input 
+            id="task_date"
+            type="date"
+            v-model="taskDate" />
         </div>
+
+        <div>
+          <label for="task_time">Hora</label>
+          <input 
+            id="task_time"
+            type="time"
+            step="300"
+            v-model="taskTime" />
+        </div>
+
 
         <div>
         <label for="task_duration">Duração</label>
@@ -27,6 +37,7 @@
             id="task_duration"
             type="number"
             step="5"
+            min="0"
             v-model="taskDuration" />
         </div>
        
@@ -36,6 +47,7 @@
             id="task_remember"
             type="number"
             step="5"
+            min="0"
             v-model="taskRemember" />
         </div>
      </div>
@@ -57,14 +69,14 @@
         <tr v-for="task in tasks" :key="task.id">
           <td>{{ task.id }}</td>
           <td>{{ task.description }}</td>
-          <td>{{ taskDateTime(task) }}</td>
+          <td>{{ taskDateTime(task.taskDateAndTime) }}</td>
           <td class="center">{{ task.minutes_duration }}</td>
           <td class="center">{{ task.remember_minutes_before }}</td>
           <td class="center">{{ taskDateChange(task) }}</td>
           <td class="center">
             <div class="action">
-              <button class="btn_edit">Editar</button>
-              <button class="btn_delete">Apagar</button>
+              <button class="btn_edit" @click="edit(task)">Editar</button>
+              <button class="btn_delete" @click="delTask(task.id)">Apagar</button>
             </div>
           </td>
         </tr>
@@ -80,7 +92,8 @@ import moment from 'moment';
 const tasks = [{
   id: 1,
   description: 'Do a pull-request',
-  datetime: new Date(),
+  task_date: new Date(),
+  task_time: new Date(),
   minutes_duration: 15,
   remember_minutes_before: 5,
   created_at: new Date(),
@@ -89,7 +102,8 @@ const tasks = [{
 },{
   id: 2,
   description: 'Go out',
-  datetime: new Date(),
+  task_date: new Date(),
+  task_time: new Date(),
   minutes_duration: 10,
   remember_minutes_before: 5,
   created_at: new Date(),
@@ -98,7 +112,8 @@ const tasks = [{
 },{
   id: 3,
   description: 'Planning a day',
-  datetime: new Date(),
+  task_date: new Date(),
+  task_time: new Date(),
   minutes_duration: 5,
   remember_minutes_before: 5,
   created_at: new Date(),
@@ -112,24 +127,40 @@ export default {
     return {
       tasks,
       taskDescription: "",
-      taskDateAndTime: "",
+      taskDate: 0,
+      taskTime: 0,
       taskDuration: 0,
       taskRemember: 0,
     }
   },
   methods: {
-    saveTask: function(e) {
+    save: function(e) {
       e.preventDefault();
-      const newId = this.tasks[this.tasks.length-1].id + 1;
+      const newId = this.tasks.length === 0 ? 1 : this.tasks[this.tasks.length-1].id + 1;
       this.tasks.push({
         id: newId,
         description: this.taskDescription,
-        datetime: this.taskDateAndTime,
+        task_date: this.taskDate,
+        task_time: this.taskTime,
         minutes_duration: this.taskDuration,
         remember_minutes_before: this.taskRemember,
         created_at: new Date()
       });
+      this.clearFields();
     },
+    edit: function(task) {
+      this.toForm(task);
+    },
+    delTask: function(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    },
+    clearFields() {
+      this.taskRemember = 0;
+      this.taskDescription = "";
+      this.taskDate = 0;
+      this.taskTime = 0;
+      this.taskDuration = 0;
+  },
     taskDateChange: function(task) {
       const dateTime = task.updated_at ?? task.created_at;
       const dataTask = this.taskDateTime(dateTime);
@@ -138,6 +169,13 @@ export default {
     taskDateTime: function(date) {
       return moment(date).format('DD/MM/yyyy HH:mm');
     },
+    toForm: function(task) {
+      this.taskRemember = task.remember_minutes_before;
+      this.taskDescription = task.description;
+      this.taskDate = moment(task.task_date).format('DD/MM/yyyy hh:mm:ss');
+      this.taskTime = moment(task.task_time).format("hh:mm:ss a")
+      this.taskDuration = task.minutes_duration;
+    }
   },
   components: {
     Header
