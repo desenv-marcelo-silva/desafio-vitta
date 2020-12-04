f8607651ce95aa62dcb8704154d08128a8be536a
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-04 12:13:27 -0300

    front-end: first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Vitta Stone</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+}
+</style>
