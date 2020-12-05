c43ee34ce863f143300400f22a8c042058b06501
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-05 20:25:24 -0300

    buid: install toaster component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import Toasted from 'vue-toasted';
+
 import "../node_modules/sakura.css/css/normalize.css"
 import "../node_modules/sakura.css/css/sakura.css"
 
 Vue.config.productionTip = false
+Vue.use(Toasted, { duration: 1500, position: 'top-center', keepOnHover: true});
 
 new Vue({
   render: h => h(App),
